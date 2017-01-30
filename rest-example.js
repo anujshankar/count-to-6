01104f5e9cd53e514a5ecff34cd7accd09af99b3
Anuj Shankar <anujshankar95@gmail.com>
2017-01-30 11:02:15 +0530

    Solved 10

--- rest-example.js
+++ rest-example.js
@@ -1,7 +1,7 @@
 function getAverage(...numbers) {
-  var sum = 0;
+  var sumOfNumbers = 0;
   numbers.forEach(function (value) {
-    sum += value;
+    sumOfNumbers += value;
   });
   var numberOfElements = numbers.length;
   var result = sum / numberOfElements;
